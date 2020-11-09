bbd7f1a87160eb14f846661739b0ea6d286a2130
Nikita Stets <mikita.stets.999@mail.ru>
2020-11-09 23:49:28 +0300

    Client Side investigation

---
+++ ASP.NET Core Check/Areas/Service/Controllers/ModelValidationController.cs
@@ -0,0 +1,27 @@
+﻿using ASP.NET_Core_Check.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Check.Areas.Service.Controllers
+{
+    public class ModelValidationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ClientSideValidationModel model, [FromQuery] string test)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var message = "product " + model.Name + " created successfully";
+
+            return Content(message);
+        }
+    }
+}
---
+++ ASP.NET Core Check/Models/ClientSideValidationModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_Core_Check.Models
+{
+    public class ClientSideValidationModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the name")]
+        [StringLength(maximumLength: 25, MinimumLength = 10, ErrorMessage = "Length must be between 10 to 25")]
+        public string Name { get; set; }
+    }
+}
